8582678e0004d499ca52882f8925daaa6c1386a2
El HATIMI Zakaria <73597675+Zack4DEV@users.noreply.github.com>
2023-12-10 03:11:08 +0100

    module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module stream.radio {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
-    requires javafx.web;
     
     opens stream.radio to javafx.fxml;
     exports stream.radio;
