ba96178544e7fe95276fbcc26ec7e8367afabb4f
El HATIMI Zakaria <73597675+Zack4DEV@users.noreply.github.com>
2023-12-10 03:35:47 +0100

    Radio.java

--- src/main/java/stream/radio/Radio.java
+++ src/main/java/stream/radio/Radio.java
@@ -48,13 +48,11 @@ public class Radio {
      name = nameButton.getText();
      button.setText("name");
     
-     button.setOnMouseClicked(new EventHandler<MouseEvent>({
-     public void handle(MouseEvent mouseEvent){
+     button.setOnMouseClicked(EventHandler<MouseEvent> handle -> {
      if(!name.getText().equals("")){
          url.setUrl((mediaURL.getText()).toURL());
          mediaPlayer.load(this.url).play();
 
-     }
      }
      });  
     }
@@ -65,43 +63,40 @@ public class Radio {
 
         StringBuilder nameButton = new StringBuilder(); 
 
-        collect.forEach(b -> nameButton.append(b).toString());  
+        collect.forEach(b -> nameButton.append(b)).toString();  
     }
      
     private  onPreloadURL() {
         
-        String stringmfm = URI.create("http://streamer.eagrpservices.com/audio/mfmradio.ogg").toString();
-        String stringaswat = URI.create("http://broadcast.ice.infomaniak.ch/aswat-high.mp3").toString();
-        String stringchada = URI.create("http://live-reflector.ice.infomaniak.ch/chadafm-high.mp3").toString();
-        String stringmars = URI.create("http://streamer.eagrpservices.com/audio/radiomars.mp3").toString();
-        String stringmedradio = URI.create("http://medradio-maroc.ice.infomaniak.ch/medradio-maroc-64.mp3").toString();
-        String stringtania = URI.create(
-                "http://cdnamd-hls-globecast.akamaized.net/live/ramdisk/radio_2m/radio_hls_ts/index.m3u8").toString();
-        String stringatlantic = URI.create("http://atlanticradio.ma:8000/stream/1/").toString();
-        String stringyabiladi = URI.create("http://snassen.yabiladi.com:8000/stream/1").toString();
-        String stringmedi = URI.create("http://51.222.88.199:80/medi1").toString();
-        String stringness = URI.create("http://radio.nessradio.net:8000/nessradio.mp3").toString();
-        String stringhitradio = URI.create("http://hitradio-maroc.ice.infomaniak.ch/hitradio-maroc-128.mp3").toString();
-        String stringhitmaroc = URI.create("http://api.webrad.io/data/streams/68/hits1-maroc").toString();
-        String stringtarab = URI.create("http://streaming2.rimradio.ma/livetarab").toString();
-        String stringwatania = URI.create(
-                "http://cdnamd-hls-globecast.akamaized.net/live/ramdisk/radio_idaa_watanya/hls_snrt_radio/index.m3u8")
-                .toString();
-        String stringmoroccoenglish = URI.create("http://streamer.radio.co/s6cba51492/listen").toString();
-        String stringtamazgha = URI.create("http://streaming2.rimradio.ma/liveamazighe").toString();
-        String stringizlan = URI.create("http://izlan.fr/radios/atlas/stream").toString();
-        String stringcoran = URI.create("http://dedi.radio-coran.net:8000/livehq.mp3").toString();
-        String stringmontecarlo = URI.create("http://montecarlodoualiya128k.ice.infomaniak.ch/mc-doualiya.mp3").toString();
-        String stringaljazeera = URI.create("http://live-hls-audio-web-aja.getaj.net/VOICE-AJA/01.m3u8").toString();
-        String stringskynewsarabia = URI.create("http://radio.skynewsarabia.com/stream/radio/skynewsarabia").toString();
-        String stringbbcarabic = URI.create("http://stream.live.vc.bbcmedia.co.uk/bbc_arabic_radio").toString();
+        String stringmfm = new String("http://streamer.eagrpservices.com/audio/mfmradio.ogg");
+        String stringaswat = new String("http://broadcast.ice.infomaniak.ch/aswat-high.mp3");
+        String stringchada = new String("http://live-reflector.ice.infomaniak.ch/chadafm-high.mp3");
+        String stringmars = new String("http://streamer.eagrpservices.com/audio/radiomars.mp3");
+        String stringmedradio = new String("http://medradio-maroc.ice.infomaniak.ch/medradio-maroc-64.mp3");
+        String stringtania = new String("http://cdnamd-hls-globecast.akamaized.net/live/ramdisk/radio_2m/radio_hls_ts/index.m3u8");
+        String stringatlantic = new String("http://atlanticradio.ma:8000/stream/1");
+        String stringyabiladi = new String("http://snassen.yabiladi.com:8000/stream/1");
+        String stringmedi = new String("http://51.222.88.199:80/medi1");
+        String stringness = new String("http://radio.nessradio.net:8000/nessradio.mp3");
+        String stringhitradio = new String("http://hitradio-maroc.ice.infomaniak.ch/hitradio-maroc-128.mp3");
+        String stringhitmaroc = new String("http://api.webrad.io/data/streams/68/hits1-maroc");
+        String stringtarab = new String("http://streaming2.rimradio.ma/livetarab");
+        String stringwatania = new String("http://cdnamd-hls-globecast.akamaized.net/live/ramdisk/radio_idaa_watanya/hls_snrt_radio/index.m3u8");
+        String stringmoroccoenglish = new String("http://streamer.radio.co/s6cba51492/listen");
+        String stringtamazgha = new String("http://streaming2.rimradio.ma/liveamazighe");
+        String stringizlan = new String("http://izlan.fr/radios/atlas/stream");
+        String stringcoran = new String("http://dedi.radio-coran.net:8000/livehq.mp3");
+        String stringmontecarlo = new String("http://montecarlodoualiya128k.ice.infomaniak.ch/mc-doualiya.mp3");
+        String stringaljazeera = new String("http://live-hls-audio-web-aja.getaj.net/VOICE-AJA/01.m3u8");
+        String stringskynewsarabia = new String("http://radio.skynewsarabia.com/stream/radio/skynewsarabia");
+        String stringbbcarabic = new String("http://stream.live.vc.bbcmedia.co.uk/bbc_arabic_radio");
 
         List<String> stringName = Arrays.asList(stringmfm,stringaswat,stringchada,stringmars,stringmedradio,stringtania,stringatlantic,stringyabiladi,stringmedi,stringness,stringhitradio,stringhitmaroc,stringtarab,stringwatania,stringmoroccoenglish,stringtamazgha,stringizlan,stringcoran,stringmontecarlo,stringaljazeera,stringskynewsarabia,stringbbcarabic);
         List<List<String>> listName =  stringName.stream().map(x -> new Scanner(x).findAll("http://").map(m -> m.group()).collect(Collectors.toList())).flatMap(List::stream).collect(Collectors.toList());
 
         StringBuilder mediaURL = new StringBuilder(); 
 
-        collect.forEach(x -> mediaURL.append(x).toString());
+        collect.forEach(x -> mediaURL.append(x)).toString();
     }
 
 }
