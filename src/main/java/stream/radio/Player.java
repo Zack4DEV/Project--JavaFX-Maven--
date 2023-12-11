be823b7783fce0d0d2e89f9cdcd0448924cf57dc
El HATIMI Zakaria <73597675+Zack4DEV@users.noreply.github.com>
2023-12-11 15:38:16 +0100

    Player.java

--- src/main/java/stream/radio/Player.java
+++ src/main/java/stream/radio/Player.java
@@ -2,39 +2,27 @@ package stream.radio;
 
 import stream.radio.Stream;
 
-import javafx.event.ActionEvent;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.Node;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
-
+import javafx.scene.media.MediaException;
 import java.io.File;
-import java.io.IOException;
 
 class Player {
 
   
+     public static void main(String[] args) throws MediaException {
+     
      private File mediaURL = new File(streamURL);
      private Media media = new Media(mediaURL.toString());     
      private MediaPlayer mediaPlayer = new MediaPlayer(media);
      private MediaView mediaView = new MediaView(mediaPlayer); 
      
-     ActionEvent actionEvent;
-  
-      public static void main(String[] args) throws IOException {
+      try{ 
+     
+        mediaPlayer.play();
       
-      try{
-      Scene scene = new Scene(new AnchorPane(mediaView), 173, 163);
-      Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-
-      stage.setScene(scene);
-      stage.show();
-  
-      mediaPlayer.play();
       } catch(IOException e){
       
         System.err.println(e);      
