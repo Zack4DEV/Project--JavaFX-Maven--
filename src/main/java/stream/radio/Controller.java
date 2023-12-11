e3ac0640afd1584b312bca4d1ca58fd6d3060a43
El HATIMI Zakaria <73597675+Zack4DEV@users.noreply.github.com>
2023-12-11 11:53:16 +0100

    Controller.java

--- src/main/java/stream/radio/Controller.java
+++ src/main/java/stream/radio/Controller.java
@@ -1,22 +1,36 @@
 package stream.radio;
+
+import stream.radio.Stream;
      
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.Media.MediaView;
+     
 import java.io.IOException;
 import java.util.Objects;
 
-class Controller {
 
+class Controller {
+     @FXML 
+     Button button = new Button();
      @FXML
-     public void Radio(ActionEvent actionEvent) throws IOException {
+     ImageView imageView = new ImageView();
+     @FXML
+     MediaView mediaView = new MediaView();
+     
+     @FXML
+     public void Stream(ActionEvent actionEvent) throws IOException {
           
           Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
           window.setScene(root);
+
           window.show();
      } 
+
+     Stream stream = new Stream();
 }
