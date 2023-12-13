677a3a81e01c1084694588d7dc549277ac9018d2
El HATIMI Zakaria <73597675+Zack4DEV@users.noreply.github.com>
2023-12-13 01:02:27 +0100

    Stream.java

--- src/main/java/stream/radio/Stream.java
+++ src/main/java/stream/radio/Stream.java
@@ -45,7 +45,7 @@ public class Stream{
 
         stringNameList.Stream().map( stringScan -> new Scanner().findAll("http://") ).map( stringScan -> stringScan.group()).flatMap().Collect(Collectors.toList());
         
-        stringNameList.forEach(String stringName -> stringScan.add(stream.radio.Stream.stringStream());        
+        stringNameList.forEach(String stringName -> stringScan.add(stream.radio.Stream.stringStream()));        
 
         
     }
