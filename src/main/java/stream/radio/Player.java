b4e1928234a594aae2212f7bb349e76123a4fa58
El HATIMI Zakaria <73597675+Zack4DEV@users.noreply.github.com>
2023-12-14 00:03:17 +0100

    Player.java

--- src/main/java/stream/radio/Player.java
+++ src/main/java/stream/radio/Player.java
@@ -4,11 +4,6 @@ import stream.radio.Data;
      
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
-import javafx.scene.media.MediaException;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
@@ -18,10 +13,6 @@ public class Player {
      
      @FXML
      private Button play = new Button();
-     @FXML
-     private MediaPlayer mediaPlayer = new MediaPlayer();
-     @FXML
-     private MediaView mediaView = new MediaView(mediaPlayer); 
   
          
     public void play(){
