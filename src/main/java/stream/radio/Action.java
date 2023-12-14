9c204792c276ab749722ebe965cd8d1e7981f811
El HATIMI Zakaria <73597675+Zack4DEV@users.noreply.github.com>
2023-12-14 14:22:13 +0100

    Action.java

--- src/main/java/stream/radio/Action.java
+++ src/main/java/stream/radio/Action.java
@@ -22,7 +22,7 @@ public class Action {
     Hyperlink hl;
     String hlString = (String) hl.toString();
 
-    Player <Ps extends Players>player = new Player();
+    <P extends Player>player = new Player();
     
     @FXML
     private Button button = new Button("");
